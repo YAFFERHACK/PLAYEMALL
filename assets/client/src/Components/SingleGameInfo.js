108a13f6a0a3d0d155c5965041f498083f86425a
Yared Santiago <yaredmyers@gmail.com>
2018-12-12 15:07:43 +0100

    cambios de preparacion

--- assets/client/src/Components/SingleGameInfo.js
+++ assets/client/src/Components/SingleGameInfo.js
@@ -1,3 +1,13 @@
 import React, { Component } from 'react';
 import AuthService from '../auth/auth-service';
 import { Link, Redirect } from 'react-router-dom'; 
+
+export default class SingleGameInfo extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- assets/server/routes/auth.js
+++ assets/server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User/User.js");
 const parser = require("../config/cloudinary");
@@ -8,126 +8,130 @@ const parser = require("../config/cloudinary");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong authenticating user" });
+      return;
+    }
 
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', (err, theUser, failureDetails) => {
-        if (err) {
-            res.status(500).json({ message: 'Something went wrong authenticating user' });
-            return;
-        }
-    
-        if (!theUser) {
-            // "failureDetails" contains the error messages
-            // from our logic in "LocalStrategy" { message: '...' }.
-            res.status(401).json(failureDetails);
-            return;
-        }
+    if (!theUser) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
 
-        // save user in session
-        req.login(theUser, (err) => {
-            if (err) {
-                res.status(500).json({ message: 'Session save went bad.' });
-                return;
-            }
+    // save user in session
+    req.login(theUser, err => {
+      if (err) {
+        res.status(500).json({ message: "Session save went bad." });
+        return;
+      }
 
-            // We are now logged in (that's why we can also send req.user)
-            res.status(200).json(theUser);
-        });
-    })(req, res, next);
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(theUser);
+    });
+  })(req, res, next);
 });
 
-router.post('/signup', (req, res, next) => {
-  const {username, password, city} = req.body;
+router.post("/signup", (req, res, next) => {
+  const { username, password, city } = req.body;
 
   if (!username || !password) {
-    res.status(400).json({ message: 'Provide username and password' });
+    res.status(400).json({ message: "Provide username and password" });
     return;
   }
 
-  if(password.length < 7){
-      res.status(400).json({ message: 'Please make your password at least 8 characters long for security purposes.' });
-      return;
+  if (password.length < 7) {
+    res
+      .status(400)
+      .json({
+        message:
+          "Please make your password at least 8 characters long for security purposes."
+      });
+    return;
   }
 
   User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      res.status(500).json({ message: "Username check went bad." });
+      return;
+    }
 
-      if(err){
-          res.status(500).json({message: "Username check went bad."});
-          return;
+    if (foundUser) {
+      res.status(400).json({ message: "Username taken. Choose another one." });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username: username,
+      password: hashPass,
+      city: city
+    });
+
+    newUser.save(err => {
+      if (err) {
+        res
+          .status(400)
+          .json({ message: "Saving user to database went wrong." });
+        return;
       }
 
-      if (foundUser) {
-          res.status(400).json({ message: 'Username taken. Choose another one.' });
+      // Automatically log in user after sign up
+      // .login() here is actually predefined passport method
+      req.login(newUser, err => {
+        if (err) {
+          res.status(500).json({ message: "Login after signup went bad." });
           return;
-      }
+        }
 
-      const salt     = bcrypt.genSaltSync(10);
-      const hashPass = bcrypt.hashSync(password, salt);
-
-      const newUser = new User({
-          username:username,
-          password: hashPass,
-          city: city,
+        // Send the user's information to the frontend
+        // We can use also: res.status(200).json(req.user);
+        res.status(200).json(newUser);
       });
-
-      newUser.save(err => {
-          if (err) {
-              res.status(400).json({ message: 'Saving user to database went wrong.' });
-              return;
-          }
-          
-          // Automatically log in user after sign up
-          // .login() here is actually predefined passport method
-          req.login(newUser, (err) => {
-
-              if (err) {
-                  res.status(500).json({ message: 'Login after signup went bad.' });
-                  return;
-              }
-          
-              // Send the user's information to the frontend
-              // We can use also: res.status(200).json(req.user);
-              res.status(200).json(newUser);
-          });
-      });
+    });
   });
 });
 
-router.get('/logout', (req, res, next) => {
+router.get("/logout", (req, res, next) => {
   // req.logout() is defined by passport
   req.logout();
-  res.status(200).json({ message: 'Log out success!' });
+  res.status(200).json({ message: "Log out success!" });
 });
 
-router.get('/loggedin', (req, res, next) => {
+router.get("/loggedin", (req, res, next) => {
   // req.isAuthenticated() is defined by passport
   if (req.isAuthenticated()) {
-      res.status(200).json(req.user);
-      return;
+    res.status(200).json(req.user);
+    return;
   }
-  res.status(403).json({ message: 'Unauthorized' });
+  res.status(403).json({ message: "Unauthorized" });
 });
-
 
 router.post("/upload", parser.single("picture"), (req, res) => {
   User.findByIdAndUpdate(req.user.id, { image: req.file.url })
-  .then(() => {
-    res.json({
-      success: true,
-      pictureUrl: req.file.url
+    .then(() => {
+      res.json({
+        success: true,
+        pictureUrl: req.file.url
+      });
+    })
+    .catch(err => {
+      res.json({
+        success: false
+      });
+      return err;
     });
-  })
-  .catch ((err)=>{
-    res.json({
-      success: false
-    });
-    return err
-  })
 });
 
-
 router.post("/edit", (req, res) => {
-  const { username, password, city} = req.body;
+  const { username, password, city } = req.body;
 
   const myUser = {};
 
@@ -139,18 +143,16 @@ router.post("/edit", (req, res) => {
     myUser.city = city;
   }
 
-//   if (course) {
-//     myUser.course = course;
-//   }
+  //   if (course) {
+  //     myUser.course = course;
+  //   }
 
-  User.findByIdAndUpdate(req.user.id, myUser).then(() => {
+  User.findByIdAndUpdate(req.user.id, myUser, { new: true }).then(user => {
     res.json({
       success: true,
-      myUser
+      myUser: user
     });
   });
 });
-
-
 
 module.exports = router;
