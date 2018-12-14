2ec61e652cb002cbc1d79118c7c196e9dd9ec96c
Yared Santiago <yaredmyers@gmail.com>
2018-12-14 10:27:41 +0100

    prepare for merge

--- assets/server/bin/seeds.js
+++ assets/server/bin/seeds.js
@@ -38,8 +38,29 @@ let users = [
     city: "Madrid",
     profileImg: "",
   },
-  
 ]
+
+let posts = [
+  {
+    title: "Yakuza 6 'After Hours Edition'",
+    content: "Vendo edición coleccionista de Yakuza 6, en perfecto estado. No acepto cambios ni bajo el precio, entrega en zona de Arguelles, Madrid",
+    price: 50,
+    picPath: "/images/subsections/exban2.png",
+    creatorId: 1,
+    // subsectionId: 4,
+    section: "Express"
+  },
+  {
+    title: "Vendo o intercambio New Nintendo 3DS",
+    content: "En perfecto estado, cambio por PS Vita o vendo, no acepto ofertas. Zona 'Callao' en Madrid",
+    price: 110,
+    picPath: "/images/subsections/exban2.png",
+    creatorId: 1,
+    // subsectionId: 4,
+    section: "Express"
+  },
+];
+
 
 User.deleteMany()
 .then(() => {
@@ -57,3 +78,41 @@ User.deleteMany()
   mongoose.disconnect()
   throw err
 })
+
+// posts.forEach((e) => {e.creatorId = usersCreated[e.creatorId]._id
+// });
+// console.log(`${usersCreated.length} users created with the following id:`);
+// console.log(usersCreated.map(u => u._id));
+
+// return Post.deleteMany()
+//       .then(() => {
+//         return Post.create(posts).catch(err => console.log(err))
+//       })
+//       .then(postsCreated => {
+//         console.log(`${postsCreated.length} posts created with the following id:`);
+//         console.log(postsCreated.map(u => u._id));
+//         comments.forEach((e) => {
+//           e.creatorId = usersCreated[e.creatorId]._id;
+//           e.postId = postsCreated[e.postId]._id
+//         }
+//         );
+//         return Comment.deleteMany()
+//           .then(() => {
+//             return Comment.create(comments).catch(err => console.log(err))
+//           })
+//           .then(commentsCreated => {
+//             console.log(`${commentsCreated.length} comments created with the following id:`);
+//             console.log(commentsCreated.map(u => u._id));
+//           })        
+//       })
+//     })  
+//   })
+//   .then(() => {
+//     // Close properly the connection to Mongoose
+//     mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     mongoose.disconnect()
+//     throw err
+//   })
+//   })
