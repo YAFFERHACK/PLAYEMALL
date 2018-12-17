449adf3c7acff4919b9eafa65cbd9a4266744b81
Yared Santiago <yaredmyers@gmail.com>
2018-12-17 18:46:51 +0100

    preparing for big merge

--- assets/client/src/Components/Nav.js
+++ assets/client/src/Components/Nav.js
@@ -26,7 +26,7 @@ export default class Nav extends Component {
         <div className="Nav">
         <nav className="navbar is-dark">
           <div className="navbar-brand">
-            <a className="navbar-item navLogo" href="https://bulma.io">
+            <a className="navbar-item navLogo" href="/">
               {" "}
               <img
                 src="https://okapy.es/image/cache/catalog/customer_designs/33-90ccb12f14fe5363fd0d270ebddec5ce-228x228.png"
