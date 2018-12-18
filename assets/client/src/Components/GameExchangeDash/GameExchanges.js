d9b62a5d487b2b7f3136671caf795c0d46de8742
Fer Perez Ayala <ferp@protonmail.ch>
2018-12-18 16:39:18 +0100

    merged from master

---
+++ assets/client/src/Components/GameExchangeDash/GameExchanges.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+import AuthService from "../../../src/auth/auth-service.js";
+import PostService from "../../auth/post-service.js";
+
+export default class GameExchanges extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: null
+    };
+    this.service = new PostService();
+  }
+
+  componentDidMount = () => {
+    this.service.dashboardShowPost().then(response => {
+      console.log(response);
+      this.setState({ ...this.state, posts: response });
+    });
+  };
+
+  render() {
+    let postSnippet = "";
+    if (this.state.posts !== null) {
+      console.log(this.props.post);
+      postSnippet = this.state.posts.map(post => {
+        return (
+          <div>
+            <img alt="imgPostSnippet" src={post.picPath} />
+            <h1>{post.title}</h1>
+            <Link to={`/completepost/${post._id}`}>
+              <button>Ver anuncio</button>
+            </Link>
+            <br />
+            <hr />
+            <br />
+            <Link to={`/newpost`}>
+              <button>Nuevo anuncio</button>
+            </Link>
+          </div>
+        );
+        // console.log(postSnippet);
+      });
+    }
+    return <div>{postSnippet}</div>;
+  }
+}
