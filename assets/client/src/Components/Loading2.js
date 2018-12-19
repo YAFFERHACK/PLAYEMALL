a02c64dde290a6a8bd7848830ad5924410e147db
Yared Santiago <yaredmyers@gmail.com>
2018-12-19 21:22:59 +0100

    started maquetacion

--- assets/client/src/Components/Loading2.js
+++ assets/client/src/Components/Loading2.js
@@ -6,7 +6,7 @@ export default class Loading2 extends Component {
   render() {
     return (
       <div className="loader2">
-        <img alt="" src="https://media.giphy.com/media/2YlelGeI5ornsi6rbC/giphy.gif" width="300" height="300"/>
+        <img className="spyroSpinner" alt="" src="https://media.giphy.com/media/2YlelGeI5ornsi6rbC/giphy.gif" width="300" height="300"/>
         <img alt="" src="https://static1.textcraft.net/data1/9/1/91ea2d0771d2393c31669086730cd4dc3ae26c8dda39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd8070947977b23192646749bbada096162022c.png" width="300" height="300"/>
       </div>
     )
