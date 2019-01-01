398ee5310aa16b729e176be0d1301b10b9f47905
Fer Perez Ayala <ferp@protonmail.ch>
2019-01-01 19:16:46 +0100

    prepared a new build production with a new component for the remove button

---
+++ assets/server/public/precache-manifest.fdcde682bd51b12bd27a97f8821078af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "399b05be72f470130d817fdfdb62a707",
+    "url": "/static/media/bg5fi.399b05be.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62fdd0d1b5fc9c74fead",
+    "url": "/static/js/main.62fdd0d1.chunk.js"
+  },
+  {
+    "revision": "96d5993b17050448cc01",
+    "url": "/static/js/1.96d5993b.chunk.js"
+  },
+  {
+    "revision": "62fdd0d1b5fc9c74fead",
+    "url": "/static/css/main.612215f9.chunk.css"
+  },
+  {
+    "revision": "96d5993b17050448cc01",
+    "url": "/static/css/1.22c7010b.chunk.css"
+  },
+  {
+    "revision": "0bc8c81c9f76b0014484f8d918fcb4fb",
+    "url": "/index.html"
+  }
+];
